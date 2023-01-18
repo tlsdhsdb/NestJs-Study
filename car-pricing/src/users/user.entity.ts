ec5f9d969039b250c96c9a4b27d6574e89173ae7
onyoo <dhsdb02@gmail.com>
2023-01-19 01:27:38 +0900

    feat(core): Add user entity

---
+++ car-pricing/src/users/user.entity.ts
@@ -0,0 +1,37 @@
+import {
+  AfterInsert,
+  AfterRemove,
+  AfterUpdate,
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @AfterInsert()
+  logInsert() {
+    console.log('Inserted User with id', this.id);
+    //this 는 여기에서 참조한 데이터이다.
+  }
+  //새사용자를 삽입할때마다 이 함수가 호출될것이다.
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log('Updated User with id', this.id);
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log('Removed User with id', this.id);
+  }
+}
